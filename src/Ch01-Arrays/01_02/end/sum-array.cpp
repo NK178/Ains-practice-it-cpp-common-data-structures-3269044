ef9ad3d2aef3764fd04cdadba570878a316fd1bc
NK178 <ayhngern@gmail.com>
2024-10-09 08:16:31 +0000

    Testing

--- src/Ch01-Arrays/01_02/end/sum-array.cpp
+++ src/Ch01-Arrays/01_02/end/sum-array.cpp
@@ -4,6 +4,21 @@ using namespace std;
 int calculateSum(const int arr[], size_t size)
 {
     //...TODO: implement
+    // Store sum of int
+    int Sum = 0;
+
+    // Check if array is not empty
+    if (size > 0)
+    {
+        // Add each element of array into Sum
+        for (int i = 0; i < size; i++)
+        {
+            Sum = Sum + arr[i];
+        }
+    }
+
+    // return sum
+    return Sum;
 }
 
 int main()
